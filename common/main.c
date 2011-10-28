97361e0224c68be8ebef3d45e921b739f207e67c
Simone Agresta <simone.agresta@bticino.it>
2011-10-28 14:32:59 +0200

    common: save environment ver
    	This patch store the ver environment variable
    	if is not set or if is different.
    
    Signed-off-by: Simone Agresta <simone.agresta@bticino.it>

--- common/main.c
+++ common/main.c
@@ -330,7 +330,12 @@ void main_loop (void)
 	{
 		extern char version_string[];
 
-		setenv ("ver", version_string);  /* set version variable */
+		s = strdup(getenv("ver"));
+
+		if ((s == NULL) || (strcmp(s, version_string))) {
+			setenv ("ver", version_string);  /* set version variable */
+			saveenv ();
+		}
 	}
 #endif /* CONFIG_VERSION_VARIABLE */
 
